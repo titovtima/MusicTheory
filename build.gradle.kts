53cf1ac8df1b36e90b78debd4bd26517f6b189e1
titovtima <titov.tima.spb@yandex.ru>
2023-11-22 15:51:02 +0300

    publish library to sonatype

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import java.util.Properties
+
 plugins {
     kotlin("multiplatform") version "1.7.21"
+    id("maven-publish")
+    id("signing")
 }
 
 group = "ru.titovtima"
@@ -43,4 +47,80 @@ kotlin {
 
 tasks.named<org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile>("compileKotlinJs").configure {
     kotlinOptions.moduleKind = "plain"
+}
+
+signing {
+    sign(publishing.publications)
+}
+
+val secretPropsFile = project.rootProject.file("local.properties")
+if (secretPropsFile.exists()) {
+    secretPropsFile.reader().use {
+        Properties().apply {
+            load(it)
+        }
+    }.onEach { (name, value) ->
+        ext[name.toString()] = value
+    }
+} else {
+    ext["signing.keyId"] = System.getenv("SIGNING_KEY_ID")
+    ext["signing.password"] = System.getenv("SIGNING_PASSWORD")
+    ext["signing.secretKeyRingFile"] = System.getenv("SIGNING_SECRET_KEY_RING_FILE")
+    ext["sonatypeUser"] = System.getenv("SONATYPE_USER")
+    ext["sonatypePassword"] = System.getenv("SONATYPE_PASSWORD")
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+fun getExtraString(name: String) = ext[name]?.toString()
+
+publishing {
+    // Configure maven central repository
+    repositories {
+        maven {
+            name = "sonatype"
+            setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = getExtraString("sonatypeUser")
+                password = getExtraString("sonatypePassword")
+            }
+        }
+    }
+
+    // Configure all publications
+    publications.withType<MavenPublication> {
+
+        // Stub javadoc.jar artifact
+        artifact(javadocJar.get())
+
+        // Provide artifacts information requited by Maven Central
+        pom {
+            name.set("MusicTheory")
+            description.set("A Kotlin library that provides several functions with music")
+            url.set("https://musictheory.titovtima.ru/")
+            issueManagement {
+                system.set("Github")
+                url.set("https://github.com/titovtima/MusicTheory/issues")
+            }
+            scm {
+                connection.set("https://github.com/titovtima/MusicTheory.git")
+                url.set("https://github.com/titovtima/MusicTheory")
+            }
+            licenses {
+                license {
+                    name.set("MIT")
+                    url.set("https://opensource.org/licenses/MIT")
+                }
+            }
+            developers {
+                developer {
+                    id.set("titovtima")
+                    name.set("Timothey Titov")
+                    email.set("titov.tima.spb@gmail.com")
+                }
+            }
+        }
+    }
 }
